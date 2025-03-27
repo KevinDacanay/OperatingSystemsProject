252e35f3866633128a440ff20f8abe0ab5c892d7
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-03-27 00:01:34 -0500

    Committing Interrupt Driver

--- src/kernel/drivers/interrupts.h
+++ src/kernel/drivers/interrupts.h
@@ -8,8 +8,11 @@ struct IDTEntry {
     uint16_t selector;
     uint8_t  zero;
     uint8_t  type_attr;
-    uint16_t base_high;
+    uint16_t base_mid;
+    uint32_t base_high;
+    uint32_t reserved;
 } __attribute__((packed));
+
 
 struct IDTPointer {
     uint16_t limit;
