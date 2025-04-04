7ed996522d597dba772275b784451836c07626ee
kienin <kevin.dacanay27@gmail.com>
2025-04-04 02:17:34 -0500

    Committing Drivers

--- src/kernel/drivers/string.h
+++ src/kernel/drivers/string.h
@@ -1,9 +1,19 @@
+// #ifndef STRING_H
+// #define STRING_H
+
+// typedef unsigned int size_t;
+
+// int strcmp(const char* str1, const char* str2);
+// int strncmp(const char* str1, const char* str2, size_t n);
+
+// #endif
 #ifndef STRING_H
 #define STRING_H
 
-typedef unsigned int size_t;
+// Define size_t as unsigned long if not already defined
+typedef unsigned long size_t;
 
 int strcmp(const char* str1, const char* str2);
 int strncmp(const char* str1, const char* str2, size_t n);
 
-#endif
+#endif // STRING_H
