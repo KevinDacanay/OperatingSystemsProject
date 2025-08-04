8dff28a29ba0fe19d7c991aaedb2ccd729768d7f
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-03 20:10:38 -0500

    Committing MeatyOS crtn

--- MeatyOS/MyOS/kernel/arch/i386/crtn.s
+++ MeatyOS/MyOS/kernel/arch/i386/crtn.s
@@ -1,3 +1,8 @@
+/*
+	crtn.s – C Runtime Termination
+	- Contains the epilogue (end) of constructor sections.
+	- Used to close out the .init and .fini sections started by crti.s.
+ */
 .section .init
     /* gcc will nicely put the contents of crtend.o's .init section here */
 	popl %ebp
