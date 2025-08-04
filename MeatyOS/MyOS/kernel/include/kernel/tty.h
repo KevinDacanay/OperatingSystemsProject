1eda343f81b14300ec05f0607d89311d3501c86e
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-03 20:11:24 -0500

    Committing MeatyOS tty header

--- MeatyOS/MyOS/kernel/include/kernel/tty.h
+++ MeatyOS/MyOS/kernel/include/kernel/tty.h
@@ -3,9 +3,16 @@
 
 #include <stddef.h>
 
+// Initializes the terminal screen (clears it).
 void terminal_initialize(void);
+
+// Writes a single character to the screen at the current cursor.
 void terminal_putchar(char c);
+
+// Writes a raw buffer to the screen.
 void terminal_write(const char* data, size_t size);
+
+// Writes a null-terminated string to the screen.
 void terminal_writestring(const char* data);
 
 #endif
