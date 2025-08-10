c82648d11aa0ba5c242c10259550b24e3b568fa8
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-10 16:05:15 -0500

    Committing MeatyOS puts

--- MeatyOS/MyOS/libc/stdio/puts.cpp
+++ MeatyOS/MyOS/libc/stdio/puts.cpp
@@ -1,5 +1,7 @@
+// --- puts.cpp ---
 #include <stdio.h>
 
+// puts: prints a null-terminated string followed by a newline
 int puts(const char* string) {
-    return printf("%s\n", string);
+    return printf("%s\n", string); // Appends newline automatically
 }
