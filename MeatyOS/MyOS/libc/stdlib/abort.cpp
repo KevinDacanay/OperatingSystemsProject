31227df428d22ac501451b3e3a5a3695f7d75146
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-10 16:05:22 -0500

    Committing MeatyOS abort

--- MeatyOS/MyOS/libc/stdlib/abort.cpp
+++ MeatyOS/MyOS/libc/stdlib/abort.cpp
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h> // Required for raising signals in user mode
 
+// Mark the function as non-returning so the compiler knows execution stops here
 __attribute__((__noreturn__))
 void abort(void) {
 #if defined(__is_libk)
-    // TO-DO: add proper kernel panic.
+    // KERNEL MODE IMPLEMENTATION
+
+    // Log a panic message to the terminal
     printf("Kernel: panic: abort()\n");
+
+    // Halt the CPU indefinitely to prevent further execution
+    asm volatile("cli");
     asm volatile("hlt");
 #else
-    // TO-DO: Abnormally terminate the process as if by SIGABRT
-    printf("abort()\n");
-#endif
-    while (1) {}
-    __builtin_unreachable();
+    // USER MODE IMPLEMENTATION
 
+    // Log the abort message (you can replace this with writing to stderr later)
+    printf("abort()\n");
+
+    // Raise the SIGABRT signal to terminate the process abnormally
+    raise(SIGABRT);
+#endif
+
+    // If for some reason execution continues, enter an infinite loop
+    while (1) {}
+
+    // Hint to the compiler that this code path is unreachable
+    __builtin_unreachable();
 }
