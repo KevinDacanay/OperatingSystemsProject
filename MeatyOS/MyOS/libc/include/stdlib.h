61183e8269618123f2c3f788e353b6c225c44133
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-09 21:38:55 -0500

    Committing MeatyOS stdlib header

--- MeatyOS/MyOS/libc/include/stdlib.h
+++ MeatyOS/MyOS/libc/include/stdlib.h
@@ -1,3 +1,4 @@
+// Declares utility functions 
 #ifndef _STDLIB_H
 #define _STDLIB_H 1
 
@@ -7,8 +8,9 @@
 extern "C" {
 #endif
 
-__attribute__((__noreturn__))
-void abort(void);
+// Causes abnormal program termination. Does not return. (kernel panic)
+__attribute__((__noreturn__))   
+void abort(void);   // fatal error handler
 
 #ifdef __cplusplus
 }
