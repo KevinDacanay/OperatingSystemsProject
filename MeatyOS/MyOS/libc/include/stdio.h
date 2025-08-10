5abc94c4b543fa241368bd0d787e4eb2de4b1d94
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-09 21:38:48 -0500

    Committing MeatyOS stdio header

--- MeatyOS/MyOS/libc/include/stdio.h
+++ MeatyOS/MyOS/libc/include/stdio.h
@@ -1,16 +1,22 @@
+// Declares standard I/O functions
 #ifndef _STDIO_H
 #define _STDIO_H 1
 
-#include <sys/cdefs.h>
+#include <sys/cdefs.h>  // Bring in internal definitions
 
-#define EOF (-1)
+#define EOF (-1)        // Standard EOF macro, returned on error by I/O functions
 
 #ifndef __cplusplus
 extern "C" {
 #endif
 
-int printf(const char* __restrict, ...);
+// Print formatted output to screen
+int printf(const char* __restrict, ...);    // __restrict is a compiler hint for optimization, it says the pointer doesn't alias others.
+
+// Print a single character
 int putchar(int);
+
+// Print a string followed by newline
 int puts(const char*);
 
 #ifndef __cplusplus
