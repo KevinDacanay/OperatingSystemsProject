192089348c7159e3e2126fda11708c3199d13d76
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-10 16:05:07 -0500

    Committing MeatyOS putchar

--- MeatyOS/MyOS/libc/stdio/putchar.cpp
+++ MeatyOS/MyOS/libc/stdio/putchar.cpp
@@ -1,15 +1,21 @@
+// --- putchar.cpp ---
 #include <stdio.h>
 
 #if defined(__is_libk)
 #include <kernel/tty.h>
 #endif
 
+// putchar: writes a single character to the output (typically the screen)
 int putchar(int ic) {
 #if defined(__is_libk)
+    // Convert integer to char and write it using kernel terminal output
     char c = (char) ic;
     terminal_write(&c, sizeof(c));
 #else
-    // TO-DO: implement stdio and write system call
-#endif 
+    // TODO: implement stdio and write system call
+    // We need to implement a syscall (e.g., write to file descriptor 1 - stdout)
+    // Example: syscall_write(1, &ic, 1);
+    // For now, we will just return the character without output
+#endif
     return ic;
 }
