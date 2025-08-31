f9ab98a2b39fb3108a7b514ce39058ea623dbd75
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-31 14:26:30 -0500

    Committing MeatyOS memmove

--- MeatyOS/MyOS/libc/string/memmove.cpp
+++ MeatyOS/MyOS/libc/string/memmove.cpp
@@ -1,14 +1,18 @@
+// Safely copies 'size' bytes from 'src' to 'dst', handling overlapping memory regions.
+
 #include <string.h>
 
 void* memmove(void* dstptr, const void* srcptr, size_t size) {
     unsigned char* dst = (unsigned char*) dstptr;
     const unsigned char* src = (const unsigned char*) srcptr;
     if (dst < src) {
+        // Safe to copy forward
         for (size_t i = 0; i < size; i++) {
             dst[i] = src[i];
         } 
     } else {
-        for (size_t i = size; i != 0; i++) {
+        // Copy backwards to avoid overwriting source
+        for (size_t i = size; i != 0; i--) {
             dst[i - 1] = src[i - 1];
         }
     }
