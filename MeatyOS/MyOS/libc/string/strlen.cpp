069f27c4543275e2f716e2fa6000d076df163b64
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-31 14:26:46 -0500

    Committing MeatyOS strlen

--- MeatyOS/MyOS/libc/string/strlen.cpp
+++ MeatyOS/MyOS/libc/string/strlen.cpp
@@ -1,9 +1,11 @@
+// Returns the number of characters in a null-terminated string 'str', excluding the null byte.
+
 #include <string.h>
 
 size_t strlen(const char* str) {
     size_t len = 0;
     while (str[len]) {
-        len++
+        len++;
     }
     return len;
 }
