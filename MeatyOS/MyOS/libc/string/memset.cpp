6d4b868524a891cd179ae6c600628a6bec84a369
Kevin Dacanay <kevin.dacanay27@gmail.com>
2025-08-31 14:26:38 -0500

    Committing MeatyOS memset

--- MeatyOS/MyOS/libc/string/memset.cpp
+++ MeatyOS/MyOS/libc/string/memset.cpp
@@ -1,6 +1,8 @@
+// Sets 'size' bytes in the memory block pointed to by 'bufptr' to the specified 'value'.
+
 #include <string.h>
 
-void *memset(void* bufptr, int value, size_t size) {
+void* memset(void* bufptr, int value, size_t size) {
     unsigned char* buf = (unsigned char*) bufptr;
     for (size_t i = 0; i < size; i++) {
         buf[i] = (unsigned char) value;
